06433f20dfe549bee60529de1506db6ca11b8a60
Linger <lingercuares@gmail.com>
2022-06-21 15:58:30 -0300

    Adding the post method to the edit route

--- main.py
+++ main.py
@@ -224,7 +224,7 @@ def add_new_post():
 
 @login_required
 @admin_only
-@app.route("/edit-post/<int:post_id>")
+@app.route("/edit-post/<int:post_id>", methods=["GET", "POST"])
 def edit_post(post_id):
     post = BlogPost.query.get(post_id)
     edit_form = CreatePostForm(
