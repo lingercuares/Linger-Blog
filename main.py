5397ebf35ba64410b2538f9d40183ab098570e22
Linger <lingercuares@gmail.com>
2022-06-21 14:21:55 -0300

    delete colon

--- main.py
+++ main.py
@@ -266,4 +266,5 @@ def delete_comment(post_id, comment_id):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
